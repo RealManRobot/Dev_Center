0578a1bcaec36894322565d84ef1876f4800dc7e
young@realman-robot.com <young@realman-robot.com>
2026-03-10 11:26:23 +0800

    AI模块的SDK开发指南，新增Lerobot下载安装包

---
+++ AI/SDK/lerobot/SDK/pyarmor_runtime_009967/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.3 (basic), 009967, 2026-03-06T18:02:40.884211
+from .pyarmor_runtime import __pyarmor__
---
+++ AI/SDK/lerobot/src/pyarmor_runtime_009967/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.3 (basic), 009967, 2026-03-06T17:57:35.579828
+from .pyarmor_runtime import __pyarmor__
